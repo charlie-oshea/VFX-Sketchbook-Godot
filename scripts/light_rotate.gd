07903b2500cc668220afc72f303c8f1a9ea70123
charlie <charlieeoshea@gmail.com>
2024-04-23 19:27:46 +0100

    - disc skybox planet

--- scripts/light_rotate.gd
+++ scripts/light_rotate.gd
@@ -1,6 +1,20 @@
 extends DirectionalLight3D
 
-@export var speed := 2.0
+@export var speed := 1
 
 func _process(delta: float) -> void:
 	rotation_degrees.x += delta * speed
+
+func _input(event):
+	if event.is_action_pressed("ui_up"):
+		speed += 1
+	if event.is_action_pressed("ui_down"):
+		speed -= 1
+	if event.is_action_pressed("ui_accept"):
+		toggle_speed()
+
+func toggle_speed():
+	if speed != 0:
+		speed = 0
+	else:
+		speed = 1
